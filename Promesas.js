f2617ff8da73b3e2f81354c10d3dee2807566020
Yad-cam <2021171017@uteq.edu.mx>
2024-01-22 17:47:57 -0600

    ejercicio4

--- Promesas.js
+++ Promesas.js
@@ -1,10 +1,21 @@
-//ejercicio3
+// 
 
-fetch('https://www.google.com.mx/?hl=es')
-.then(resp => resp.text())
-.then(html => {
-    document.open()
-    document.write(html)
-    document.close()
+let user  ={
+    name: "Yad",
+    age: 21
+}
+
+fetch('https://reqres.in/api/users', {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: {
+        'Content-Type': 'application/json'
+    }
 })
 
+.then(resp => resp.json())
+.then(console.log)
+.catch(error => {
+    console.log('error en la peticion');
+    console.log(error)
+;})
