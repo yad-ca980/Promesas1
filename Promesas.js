1f43b0d68fbb6021c62e1976ec25e26141760dfc
Yad-cam <2021171017@uteq.edu.mx>
2024-01-22 17:59:15 -0600

    ,,,

--- Promesas.js
+++ Promesas.js
@@ -1,21 +1,3 @@
 // 
 
-let user  ={
-    name: "Yad",
-    age: 21
-}
 
-fetch('https://reqres.in/api/users', {
-    method: 'POST',
-    body: JSON.stringify(user),
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
-
-.then(resp => resp.json())
-.then(console.log)
-.catch(error => {
-    console.log('error en la peticion');
-    console.log(error)
-;})
