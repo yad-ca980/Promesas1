41f34c36178535643c46000542ba8a4b0f3f0c72
Yad-cam <2021171017@uteq.edu.mx>
2024-01-22 11:24:28 -0600

    Ejercici1 de promesas

--- Promesas.js
+++ Promesas.js
@@ -1,12 +1,9 @@
+
+// Promesas 1
+
 let promesaExitosa = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve("Promesa exitosa");
     }, 3000);
 });
 
-
-promesaExitosa.then((mensaje) => {
-    console.log(mensaje);
-}).catch((error) => {
-    console.error(error);
-});
