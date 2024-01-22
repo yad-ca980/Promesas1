e3ecd30ff0b81c81349c54bcd2151a07491a1457
Yad-cam <2021171017@uteq.edu.mx>
2024-01-22 11:35:00 -0600

    Promesas6

--- Promesas.js
+++ Promesas.js
@@ -10,3 +10,18 @@ const sumarLento = (numero) => {
         }
     )
 }
+
+const sumarRapido = (numero) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(numero + 1);
+            //reject("Error")
+        }, 800);
+    }
+    )
+}
+//Compite con todas las promesas y hace la que se ejecuta más rápido
+Promise.race(([ sumarLento(5),sumarRapido(6)]))
+.then((resultados) => {
+    console.log(resultados);
+}).catch(console.log);
