7a0d378d4af489a54ecb9f5738015d7e81f919e8
Yad-cam <2021171017@uteq.edu.mx>
2024-01-22 14:19:01 -0600

    Promesas

--- Promesas.js
+++ Promesas.js
@@ -1,42 +1,24 @@
-//Promesa 7
+function promesaConTimeout(promesa, tiempoLimite) {
+    const promesaConTimeout = new Promise((resolve, reject) => {
+      const temporizador = setTimeout(() => {
+        reject("Tiempo agotado");
+      }, tiempoLimite);
+  
+      promesa.then((resultado) => {
+        clearTimeout(temporizador);
+        resolve(resultado);
+      }).catch((error) => {
+        clearTimeout(temporizador);
+        reject(error);
+      });
+    });
+  
+    return promesaConTimeout;
+}
 
-// Crea una función realizarOperacionesAsincronas que toma un array de funciones que devuelven promesas.
-//  La función debe ejecutar estas operaciones de forma secuencial, resolviendo cada promesa antes de pasar a la siguiente. 
-//  Si alguna promesa es rechazada, la función debe detenerse y rechazar con el motivo.
+const promesaLenta = new Promise((resolve) => setTimeout(() => resolve("¡Promesa resuelta!"), 2000));
+const tiempoLimite = 1000;
 
-async function realizarOperacionesAsincronas(arrayDeFunciones) {
-    try {
-      await Promise.all(arrayDeFunciones.map(funcion => funcion()));
-    } catch (error) {
-      throw error;
-    }
-  }
-  
-  const Promesa1 = () => new Promise(
-    (resolve) => setTimeout(() =>
-      resolve('Promesa 1 completa'), 900)
-  );
-//   const Promesa2 = () => new Promise(
-//     (resolve, reject) => setTimeout(() =>
-//       resolve('Promesa 2 completa'), 800)
-//   );
-  
-  const Promesa2 = () => new Promise(
-    (resolve, reject) => setTimeout(() =>
-      reject('Promesa 2 FALLIDA'), 800)
-  );
-  
-  const Promesa3 = () => new Promise(
-    (resolve) => setTimeout(() =>
-      resolve('Promesa 3 completa'), 700)
-  );
-  
-  const arrayDeFunciones = [Promesa1, Promesa2, Promesa3];
-  
-  realizarOperacionesAsincronas(arrayDeFunciones)
-    .then(() => console.log('Todas las operaciones completadas'))
-    .catch((error) => console.error('Error:', error));
-  
-
-//PRETIER
-//SONARLINE
+promesaConTimeout(promesaLenta, tiempoLimite)
+    .then((resultado) => console.log(resultado))
+    .catch((error) => console.error("Error:", error));
