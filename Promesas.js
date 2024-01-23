c38adb0413371dd46589f44ea70dbe67f6cebe37
Yad-cam <2021171017@uteq.edu.mx>
2024-01-22 18:00:47 -0600

    Ejercicio5

--- Promesas.js
+++ Promesas.js
@@ -1,3 +1,7 @@
-// 
+// Almacena de manera local una imagen dentro de este Proyecto
+// en la carpeta img realiza una solicitud fetch a 
+// esa imagen Muestra la imagen en el navegador
 
-
+const img = document.createElement('img');
+const body = document.querySelector('body');
+body.append(img);
