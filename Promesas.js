49cf0274d64dc1536f718a20ffc7ef4a7c2cc0ce
Yad-cam <2021171017@uteq.edu.mx>
2024-01-24 17:13:14 -0600

    ejercicio6

--- Promesas.js
+++ Promesas.js
@@ -1,27 +1,7 @@
-// Almacena de manera local una imagen dentro de este Proyecto
-// en la carpeta img realiza una solicitud fetch a 
-// esa imagen Muestra la imagen en el navegador
+// Ejercicio6 
+//Solicitud a una API fetch
 
-const rutaImagenLocal = './img/Map.png';
-
-function mostrarImagen() {
-  fetch(rutaImagenLocal)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`No se pudo cargar la imagen. Estado: ${response.status}`);
-      }
-      return response.blob();
-    })
-    .then(blob => {
-      const imageUrl = URL.createObjectURL(blob);
-      const imagen = new Image();
-      imagen.src = imageUrl;
-
-      document.body.appendChild(imagen);
-    })
-    .catch(error => {
-      console.error('Error al cargar la imagen:', error);
-    });
-}
-mostrarImagen();
+fetch("https://fakestoreapi.com/products/1")
+.then(resp  => resp.json())
+.then(data => console.log(data))
 
