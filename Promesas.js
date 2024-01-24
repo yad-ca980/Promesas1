0f4a81d8f1267a89188a8aba5d176963864442bf
Yad-cam <2021171017@uteq.edu.mx>
2024-01-24 17:47:21 -0600

    Ejercicio incompleto

--- Promesas.js
+++ Promesas.js
@@ -1,33 +1,38 @@
-// Ejercicio6 
-//Solicitud a una API fetch
 
+
+// // Ejercicio7
 // fetch("https://fakestoreapi.com/products/1")
 // .then(resp  => {
+
 //   resp.json().then(data => {
 //     console.log(data);
 //   })
-//   resp.json().then(data => {
-//     console.log(data);
+//   .catch(err => {
+//     console.log("Error en la petición")
+//     console.log(err);
 //   })
-// }
+// })
 
-//   )
 
-// Ejercicio7
-fetch("https://fakestoreapi.com/products/1")
-.then(resp  => {
-  resp.clone().json().then(data => {
-    console.log(data);
-  })
-  resp.clone().json().then(data => {
-    console.log(data);
-  })
-  resp.json().then(data => {
-    console.log(data);
-  })
-}
+// ejercicio7.1
 
-  )
+fetch("https://rickandmortyapi.com/api/character/800")
+  .then(resp => {
+    if (resp.ok) {
+      return resp.json();
+    }
+    else {
+      throw new Error("No se pudo completar la petición")
+    }
+  })
+  .then(console.log)
+  .catch(err => {
+    console.log("Error en la petición")
+    console.log(err)
+  });
+
+
+
 
 
 
